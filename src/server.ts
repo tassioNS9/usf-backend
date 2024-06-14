8574ce5a0410e7445acf71552d8bc181cc84ce8c
Tassio Neves <88889043+tassioNS9@users.noreply.github.com>
2024-06-13 22:07:32 -0300

    teste rotas

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,22 @@ import  Express, { NextFunction, Request, Response } from "express";
 import cors from "cors"
 import router from "./routes";
 import UserController from "./controllers/UserController";
-
+import prisma from "./database";
 const app = Express();
 
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT ?? 3333
 
 app.use(Express.json())
 app.use(cors())
 app.use(router)
 
-router.get('/api/users', UserController.getUsers)
+//router.get('/api/users', UserController.getUsers)
+
+app.get('/userslist', async (request, response) => {
+    const users= await prisma.user.findMany()
+    return response.json(users)
+  })
 
 app.use((error: Error, request: Request, response: Response, next:NextFunction)=>{
     return response.json({
