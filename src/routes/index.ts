85b476efbaa6eb2af27947454065928f9ceff8c4
Tassio Neves <88889043+tassioNS9@users.noreply.github.com>
2024-06-13 21:25:05 -0300

    removendo rotas privadas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,7 +12,7 @@ const refreshTokenController = new RefreshTokenController()
 
 //unit
 router.post('/api/units', AuthMiddlewares, UnitController.createUnit)
-router.get('/api/units', AuthMiddlewares, UnitController.getUnits)
+router.get('/api/units', UnitController.getUnits)
 router.get('/api/units/search', AuthMiddlewares, UnitController.filteredUnits)
 router.get('/api/units/:id', AuthMiddlewares, UnitController.getUnitById)
 router.put('/api/units/:id', AuthMiddlewares, UnitController.updateUnit)
@@ -24,7 +24,7 @@ router.post('/api/auth', AuthControler.authUser)
 router.get('/api/auth/verify', AuthControler.authverifyUser)
 router.post('/api/refresh-token', refreshTokenController.handle)
 router.post('/api/users', AuthMiddlewares, UserController.createUser)
-router.get('/api/users', AuthMiddlewares, UserController.getUsers)
+router.get('/api/users', UserController.getUsers)
 router.get('/api/users/search', AuthMiddlewares, UserController.filteredUsers)
 router.get('/api/users/:id', AuthMiddlewares, UserController.getUserById)
 router.put('/api/users/:id', AuthMiddlewares, UserController.updateUser)
@@ -42,7 +42,7 @@ router.delete('/evaluations/:id', AuthMiddlewares, EvaluationController.deleteEv
 
 //indicators
 router.post('/api/indicators',AuthMiddlewares, IndicatorController.createIndicator )
-router.get('/api/indicators', AuthMiddlewares, IndicatorController.getIndicators)
+router.get('/api/indicators', IndicatorController.getIndicators)
 router.get('/api/indicators/search', AuthMiddlewares, IndicatorController.filteredIndicators)
 router.get('/api/indicators-numerics/:id_unit/:year', AuthMiddlewares, IndicatorController.getIndicatorsNumericsByUnit)
 router.get('/api/indicators/:id', AuthMiddlewares, IndicatorController.getIndicatorById)
